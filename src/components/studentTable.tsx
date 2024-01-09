906c33757173e09252b1ab772fc7c1d36c9b47c1
Salomao-Faleta <faletas41@gmail.com>
2024-01-09 03:31:17 -0300

    Exercício: Tabela de notas (2/2)
    Exercício focado na prática de props, recebendo dados externos e algumas outras funcionalidades

--- src/components/studentTable.tsx
+++ src/components/studentTable.tsx
@@ -17,7 +17,28 @@ export function StudentTable({ students }:Props){
                 </tr>
             </thead>
             <tbody>
+                {students.map(item => (
+                    <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+                        <td className="p-3 flex items-center">
+                            <img src={item.avatar} alt={item.name} className="w-10 h-10 rounded-full mr-3"/>
+                            <div>
+                                <div className="font-bold">{item.name}</div>
+                                <div>{item.email}</div>
+                            </div>
+                        </td>
 
+                        <td className="p-3">
+                            {item.active && <div className=" px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-sm">Active</div>}
+                            {!item.active && <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-sm">Inactive</div>}
+                        </td>
+
+                        <td className="p-3 text-center">{item.grade1.toFixed(1)}</td>
+                        <td className="p-3 text-center">{item.grade2.toFixed(1)}</td>
+                        <td className="p-3 text-center font-bold">{item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--'}</td>
+
+
+                    </tr>
+                ))}
             </tbody>
         </table>
     )

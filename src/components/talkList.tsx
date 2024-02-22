e6e1119d346b7887b631d0339ba13e95a4871108
Salomao-Faleta <faletas41@gmail.com>
2024-02-21 21:47:47 -0300

    Adicionando mensagens

--- src/components/talkList.tsx
+++ src/components/talkList.tsx
@@ -5,23 +5,38 @@ import { useState } from "react";
 
 export function TalkList() {
 
+    const [itemInput, setItemInput] = useState('');
+
     const [list, setList] = useState<TodoItem[]>([
         { label: "Estudar React", checked: false },
         { label: "Estudar músicas do neojiba", checked: false }
     ]);
 
+    function handleAddtask(){
+        setList([
+            ...list,
+            {label: itemInput, checked: false}
+        ]);
+
+        setItemInput('');
+    }
+
     return (
-        <div className="w-screen h-screen flex flex-col items-center justify-center text-3xl">
+        <div className="w-screen h-screen flex flex-col items-center justify-center text-2xl">
             <h1 className="text-4xl mt-5">Talk List</h1>
 
             <div className="flex w-full max-w-lg m-3 p-4 rounded-md bg-gray-700 border-2 border-gray-500">
                 <input type="text"
                     placeholder="O que fazer?"
                     className="flex-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
+                    value={itemInput}
+                    onChange={e => setItemInput(e.target.value)}
                 />
 
-                <button className="text-white">Adicionar</button>
+                <button onClick={handleAddtask} className="text-white">Adicionar</button>
             </div>
+
+            <p className="my-4">{list.length} Itens na lista</p>
 
             <ul className="w-full max-w-lg list-disc pl-5">
                 {list.map(item => (
